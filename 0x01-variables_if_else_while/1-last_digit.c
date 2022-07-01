e913799915f96ec8faf50c778c9e57fa8232be2b
abiola814 <oladunjoye1998@gmail.com>
2022-07-01 16:07:11 -0700

    pop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("Last digit of %d is %d",n,(n % 10));
 	if ((n % 10)>5)
 	{
-		printf(" and greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
 	else if ((n%10)==0)
 	{

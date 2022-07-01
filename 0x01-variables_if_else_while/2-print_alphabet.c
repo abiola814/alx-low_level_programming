2c0c46917f6df1291687fa9871d7da2d1c146d9a
abiola814 <oladunjoye1998@gmail.com>
2022-07-01 15:41:15 -0700

    pop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/** more headers goes there 
+ *
+*betty style doc for function main goes there 
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++
+	}
+	putchar('\n');
+	return (0)
+}

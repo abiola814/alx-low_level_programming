f38a9c33bec1182367e5dfd2d3bd0c37d4f5972f
metamask <mike43lost@gmail.com>
2022-07-05 22:16:34 +0100

    c function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * use putchar to print putchar and a new line
+ * Return: zero to confirm it was successful
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/** header for c files created in 
+*
+* includ header file
+*/
+#include <stdio.h>
+
+void _putchar();

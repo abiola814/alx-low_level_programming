bb6d2a98aa18128406f9787a4fe848e64741f28a
metamask <mike43lost@gmail.com>
2022-07-06 00:47:21 +0100

    c update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,4 @@
-#include <unistd.h>
-
-/**
-* _putchar - writes the character c to stdout
-* print_alphabet - prints alphabet and returns a new line
-* print_alphabet_x10 - print alphabet 10 times
-* _islower - return 1 if c is lowercase, and 0 otherwise
-* _isalpha - return 1 if c is a letter, upper or lowercase, 0 otherwise
-* print_sign - return 1 for +, 0 for 0, -1  for -
-* _abs - computes absolute value of integer
-* print_last_digit - prints last digit
-* jack_bauer - prints all the minutes of his day
-* times_table - prints times table up to 9
-* add - add two ints, return sum
-* print_to_98 - prints all natural numbers to 98
-* print_times_table - prints table table up to 15
-*/
-int _putchar(char);
+int _putchar(char i);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);

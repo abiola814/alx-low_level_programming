5c6567fc01b6ed08139dd33f16f92d6237181242
unholy <you@youraddress.com>
2022-07-29 23:38:49 +0100

    static library

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,7 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-/*copy list from task*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /*"HOLBERTON_H*/
